0d69ea870939a4803a4dfe8f089d5e8628618437
cygnus <whenthefoundationcrumbles@gmail.com>
2023-10-21 22:59:14 +0300

    sdf

--- spook/script.js
+++ spook/script.js
@@ -1,6 +1,6 @@
 OWOP.options.eventoggle = localStorage.getItem("evtoggle");
 OWOP.util.toggleevent = () =>{
-	OWOP.options.eventoggle = localStorage.getItem("evtoggle") == 1 ? 0 : 1;
+	OWOP.options.eventoggle = localStorage.getItem("evtoggle") == 0 ? 0 : 1;
 	localStorage.setItem("evtoggle", OWOP.options.eventoggle);
 	alert(`triggered button, toggle state ${OWOP.options.eventtoggle}`);
 }
