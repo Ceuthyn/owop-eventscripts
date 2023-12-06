23547d6ff7d8bad2596dfbea2fcca8f1d5bd04df
cygnus <whenthefoundationcrumbles@gmail.com>
2023-12-06 05:59:18 +0200

    kms

--- festive/script.js
+++ festive/script.js
@@ -3,7 +3,7 @@ OWOP.options.eventoggle = localStorage.getItem("evtoggle");
 OWOP.util.toggleevent = ()=>{ //literally just the thing for the button
 	OWOP.options.eventoggle = localStorage.getItem("evtoggle")==0 || localStorage.getItem("evtoggle")==undefined? 1 : 0;
 	localStorage.setItem("evtoggle", OWOP.options.eventoggle);
-	OWOP.chat.local(${OWOP.options.eventoggle ? "enabled festivities! may have an impact on performance" : "disabled festivities."});
+	OWOP.chat.local(OWOP.options.eventoggle ? "enabled festivities! may have an impact on performance" : "disabled festivities.");
 	if(OWOP.options.eventoggle){
 		//enable bullshit
 		OWOP.util.enablevnt();
