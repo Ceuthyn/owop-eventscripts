52014f9c277b3c29ad4e73d6d38b6a689972be48
cygnus <whenthefoundationcrumbles@gmail.com>
2023-12-06 10:30:01 +0200

    aa

--- festive/script.js
+++ festive/script.js
@@ -74,7 +74,7 @@ class snow{
 
 	update(){ 
 		this.y += window.innerHeight*0.01;
-		console.log(this.noise(this.y/window.innerHeight/25))
+		console.log(this.noise(this.y/window.innerHeight*25)*5)
 		this.x += this.direction + (this.noise(this.y/window.innerHeight/25));
 		this.ele.style.left = this.x+"px";
 		this.ele.style.top = this.y+"px";
