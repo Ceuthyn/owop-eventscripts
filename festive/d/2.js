0b2e8c927538dfba258cf3ef7c74026bf0bbf395
cygnus <whenthefoundationcrumbles@gmail.com>
2023-12-12 21:13:43 +0200

    fucking

--- festive/d/2.js
+++ festive/d/2.js
@@ -1,8 +1,10 @@
 if(!OWOP.util.d2t){
 	OWOP.chat.local("the second, a bit of novely");
+	OWOP.util.d2t = true;
 	let canvas = OWOP.elements.animCanvas;
 	let ctx = canvas.getContext("2d");
 	function render() {
+		if(!OWOP.util.d2t) return;
 		OWOP.renderer.render(OWOP.renderer.rendertype.FX);
 		let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 		let date = Date.now();
@@ -25,9 +27,8 @@ if(!OWOP.util.d2t){
 		ctx.putImageData(imageData, 0, 0);
 		window.requestAnimationFrame(render);
 	}
-	OWOP.util.d2t = window.requestAnimationFrame(render);
+	window.requestAnimationFrame(render);
 } else {
 	OWOP.chat.local("some would rather remain without");
-	window.cancelAnimationFrame(OWOP.util.d2t);
-	delete OWOP.util.d2t;
+	OWOP.util.d2t = false;
 }
