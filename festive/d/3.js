5ecbcc412ba955c285af61bf3f617689afe0b07a
cygnus <whenthefoundationcrumbles@gmail.com>
2023-12-13 23:25:30 +0200

    fix image, change names

--- festive/d/3.js
+++ festive/d/3.js
@@ -1,7 +1,7 @@
 OWOP.util.funniwindow = ()=>{
-	OWOP.windowSys.addWindow( new OWOP.windowSys.class.window(Math.random(),{closeable: true},(t)=>{
+	OWOP.windowSys.addWindow( new OWOP.windowSys.class.window(Math.floor(Math.random()*1000000),{closeable: true},(t)=>{
 		t.frame.style.width = "100px";
-		t.container.innerHTML = `<img src="https://ceuthyn.github.io/owop-eventscripts/img/meat.jpg" style="width: 100%">`;
+		t.container.innerHTML = `<img src="https://ceuthyn.github.io/owop-eventscripts/festive/img/meat.jpg" style="width: 100%">`;
 		t.onclose = ()=>{OWOP.util.funniwindow(); OWOP.util.funniwindow()}
 	}
 	).move(Math.random()*(window.innerWidth-100) + 50, Math.random()*(window.innerHeight-100) + 50))
