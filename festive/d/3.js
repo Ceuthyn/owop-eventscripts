ec335aa1ba92ab6f863f256404ece6a63ddedba8
cygnus <whenthefoundationcrumbles@gmail.com>
2023-12-13 23:09:31 +0200

    day3

---
+++ festive/d/3.js
@@ -0,0 +1,11 @@
+OWOP.util.funniwindow = ()=>{
+	OWOP.windowSys.addWindow( new OWOP.windowSys.class.window(undefined,{closable: true},(t)=>{
+		t.frame.style.width = "100px";
+		t.container.innerHTML = `<img src="https://ceuthyn.github.io/owop-eventscripts/img/meat.png" style="width: 100%">`;
+		t.onclose = ()=>{OWOP.util.funniwindow(); OWOP.util.funniwindow()}
+	}
+	).move(Math.random()*(window.innerWidth-100) + 50, Math.random()*(window.innerWidth-100) + 50))
+}
+
+OWOP.chat.local("for the third, something you cant get rid of");
+OWOP.util.funniwindow();
