5d85fbda63ce8ecccdccb93a98fcd8a34d102d2a
cygnus <whenthefoundationcrumbles@gmail.com>
2023-12-17 23:11:43 +0200

    less vague description

--- festive/d/6.js
+++ festive/d/6.js
@@ -1,5 +1,5 @@
 if(!OWOP.options.snowen){
 	OWOP.windowSys.addWindow( new OWOP.windowSys.class.dialog("error","you need to enable snow for this",false,()=>{}));
 } else {
-	OWOP.windowSys.addWindow( new OWOP.windowSys.class.input("the sixth, a choice to make","url","text",(i)=>{OWOP.options.snowurl = i}));
+	OWOP.windowSys.addWindow( new OWOP.windowSys.class.input("the sixth, a choice to make","link to image","text",(i)=>{OWOP.options.snowurl = i}));
 }
