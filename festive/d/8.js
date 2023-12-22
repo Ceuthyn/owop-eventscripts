72b1420e1d11524c58ee0d5d99fc9628c4233403
cygnus <whenthefoundationcrumbles@gmail.com>
2023-12-22 20:36:52 +0200

    idk

--- festive/d/8.js
+++ festive/d/8.js
@@ -1,1 +1,23 @@
-OWOP.chat.local("still behind schedule")
+if(!OWOP.util.d2t3){
+	OWOP.chat.local("eight, become (color)blind");
+	OWOP.util.d2t3 = true;
+	let canvas = OWOP.elements.animCanvas;
+	let ctx = canvas.getContext("2d");
+	function render() {
+		OWOP.renderer.render(OWOP.renderer.rendertype.FX);
+		let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+		for(let i = 0; i<imageData.length; i+=4){
+			imageData[i] = 255; 	
+		}
+		ctx.putImageData(imageData, 0, 0);
+		let g = window.requestAnimationFrame(render);
+		if(!OWOP.util.d2t2){
+			window.cancelAnimationFrame(g);
+			return;
+		}
+	}
+	window.requestAnimationFrame(render);
+} else {
+	OWOP.chat.local("behold, green returns");
+	OWOP.util.d2t3 = false;
+}
