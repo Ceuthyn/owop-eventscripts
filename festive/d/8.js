e4f41970ffe3d95f947c9dde1c923d672d79ab68
cygnus <whenthefoundationcrumbles@gmail.com>
2023-12-22 22:48:02 +0200

    change way red channel is replaced

--- festive/d/8.js
+++ festive/d/8.js
@@ -7,7 +7,7 @@ if(!OWOP.util.d2t3){
 		OWOP.renderer.render(OWOP.renderer.rendertype.FX);
 		let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 		for (let i=0; i<imageData.data.length; i+=4) {
-			imageData.data[i + 0] = 127;
+			imageData.data[i + 0] = (imageData.data[i + 1] + imageData.data[i + 2])/2;
 		}
 		ctx.putImageData(imageData, 0, 0);
 		let g = window.requestAnimationFrame(render);
