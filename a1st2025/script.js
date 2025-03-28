34a2bc2551d4c40066a4a6425ea1c2091f6a1c83
cygnus <whenthefoundationcrumbles@gmail.com>
2025-03-28 19:18:24 +0200

    temp removal of crack

--- a1st2025/script.js
+++ a1st2025/script.js
@@ -54,42 +54,13 @@ setTimeout(() => {
 	
 	crunches = [];
 
-	OWOP.util.procmove = ()=>{
-		for(let i = 0; i<crunches.length; i++){
-			let sc = OWOP.camera.zoom / 16 * crunches[i].s;
-			let tx = ((-OWOP.camera.x + crunches[i].x) * OWOP.camera.zoom);
-			let ty = ((-OWOP.camera.y + crunches[i].y) * OWOP.camera.zoom);
-			if(tx > -(crunches[i].el.offsetHeight * crunches[i].s) * sc && -(crunches[i].el.offsetWidth * crunches[i].s) * sc && tx < window.innerwidth+crunches[i].el.offsetWidth && ty < window.innerHeight+crunches[i].el.offsetHeight){
-				elem.style.transform = `matrix(${sc},0,0,${sc},${Math.round(tx)},${Math.round(ty)})`;
-				if(crunches[i].sh == false){
-					OWOP.elements.viewport.appendChild(crunches[i].el);
-					crunches[i].ch = true;
-				}
-			} else {
-				if(crunches[i].sh == true){
-					crunches[i].el.remove();
-				}
-				crunches.splice(i,1);
-				i--;
-			}
-		}
-	};
-
-	OWOP.util.placec = (x,y,s)=>{
-		let el = document.createElement("div");
-		el.innerHTML = "<img src='https://ceuthyn.github.io/owop-eventscripts/a1st2025/assets/crack.png'>";
-		el.style.transformOrigin = "center";
-		el.style.position = "fixed";
-		el.style.imageRendering = "pixelated";
-		crunches.push({x,y,s,r:Math.random()*360, el, sh: false});
-	};
 
 	let crunch = new Audio("https://ceuthyn.github.io/owop-eventscripts/a1st2025/assets/crunch.mp3");
 	OWOP.tools.addToolObject(new OWOP.tools.class("ban hammer", OWOP.cursors.ban, OWOP.fx.player.NONE, OWOP.RANK.USER, (tool)=>{
 		tool.setEvent("mousedown", mouse=>{
 			if(mouse.buttons == 4) return;			
 				OWOP.util.abberation(Math.ceil(Math.random()*15 + 10));
-				OWOP.util.placec(OWOP.mouse.tileX, OWOP.mouse.tileY, OWOP.camera.zoom);
+				//OWOP.util.placec(OWOP.mouse.tileX, OWOP.mouse.tileY, OWOP.camera.zoom);
 				
 				crunch.play();
 				//do abberation and crack here
