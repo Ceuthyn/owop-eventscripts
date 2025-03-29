e6c21a5e7cae9eff5c337f3a8555330894f1f080
cygnus <whenthefoundationcrumbles@gmail.com>
2025-03-29 19:22:39 +0200

    add fake paste

--- a1st2025/script.js
+++ a1st2025/script.js
@@ -26,6 +26,8 @@ setTimeout(() => {
 	let crunch = new Howl({
 		src: ["https://ceuthyn.github.io/owop-eventscripts/a1st2025/assets/crunch.mp3"]
 	});
+
+
 	OWOP.util.framec = 0;
 	aberact = 0;
 	let rend = ()=>{
@@ -63,7 +65,7 @@ setTimeout(() => {
 	OWOP.util.stickymov = ()=>{
 		for(let i = 0; i<stickyel.length; i++){
 			let it = stickyel[i];
-			let sc = OWOP.camera.zoom / (16 * it.z);
+			/*let sc = OWOP.camera.zoom / 16 * it.z;
 			let tx = ((-OWOP.camera.x +it.x) * OWOP.camera.zoom);
 			let ty = ((-OWOP.camera.y +it.y) * OWOP.camera.zoom);
 			console.log(sc,tx,ty)
@@ -89,7 +91,32 @@ setTimeout(() => {
 					it.el.remove();
 					it.sh = false;
 				}
+			}*/
+
+			let sc = OWOP.camera.zoom / (16 * it.z);
+			let tx = ((-OWOP.camera.x +it.x) * OWOP.camera.zoom);
+			let ty = ((-OWOP.camera.y +it.y) * OWOP.camera.zoom);
+			if (tx > -(it.el.offsetHeight*it.z) * sc && ty > -(it.el.offsetWidth*it.z) * sc && tx < window.innerWidth && ty < window.innerHeight) {
+				if (sc > 1.0 && !it.ismag) {
+					it.ismag = true;
+					it.el.style.imageRendering = 'pixelated';
+				} else if (sc <= 1.0 && it.ismag) {
+					it.ismag = false;
+					it.el.style.imageRendering = 'auto';
+				}
+
+				it.el.style.transform ='matrix(' + sc + ',0,0,' + sc + ',' + Math.round(tx) + ',' + Math.round(ty) + ')';
+				if (!it.sh) {
+					OWOP.elements.viewport.appendChild(it.el);
+					it.sh = true;
+				}
+			} else {
+				if (it.sh) {
+					it.el.remove();
+					it.sh = false;
+				}
 			}
+
 		}
 	};
 	OWOP.on(OWOP.events.camMoved, OWOP.util.stickymov);
@@ -110,7 +137,6 @@ setTimeout(() => {
 	};
 	
 	OWOP.util.cdeacyint = setInterval(OWOP.util.cdecay,1000);
-//x, y, zoom, el, sh, ismag, type, {extra}
 
 	let addcrack = ()=>{
 		let div = document.createElement("div");
@@ -143,6 +169,268 @@ setTimeout(() => {
 				crunch.play();
 		});
 	}));
+
+	//fake copy/paste
 	
+	let q;
+	let rip = new Howl({
+		src: ["https://ceuthyn.github.io/owop-eventscripts/a1st2025/assets/paperrip.mp3"],
+		
+	});
+	let smack = new Howl({
+		src: ["https://ceuthyn.github.io/owop-eventscripts/a1st2025/assets/papersmack.mp3"],
+	});
+	rip.on("end",()=>{
+		if(q == undefined) return;
+		console.log("ripped");
+		stickyel.push(q);
+		smack.rate(Math.random()*0.1 + 0.9);
+		smack.play();
+		OWOP.util.stickymov();
+	});
+	
+	OWOP.tools.addToolObject(new OWOP.tools.class('Copys', OWOP.cursors.copy, OWOP.fx.player.NONE, OWOP.RANK.USER, function (tool) {
+		function drawText(ctx, str, x, y, centered) {
+			ctx.strokeStyle = "#000000", ctx.fillStyle = "#FFFFFF", ctx.lineWidth = 2.5, ctx.globalAlpha = 0.5;
+			if (centered) {
+				x -= ctx.measureText(str).width >> 1;
+			}
+			ctx.strokeText(str, x, y);
+			ctx.globalAlpha = 1;
+			ctx.fillText(str, x, y);
+		}
+
+		tool.setFxRenderer(function (fx, ctx, time) {
+			if (!fx.extra.isLocalPlayer) return 1;
+			var x = fx.extra.player.x;
+			var y = fx.extra.player.y;
+			var fxx = (Math.floor(x / 16) - OWOP.camera.x) * OWOP.camera.zoom;
+			var fxy = (Math.floor(y / 16) - OWOP.camera.y) * OWOP.camera.zoom;
+			var oldlinew = ctx.lineWidth;
+			ctx.lineWidth = 1;
+			if (tool.extra.end) {
+				var s = tool.extra.start;
+				var e = tool.extra.end;
+				var x = (s[0] - OWOP.camera.x) * OWOP.camera.zoom + 0.5;
+				var y = (s[1] - OWOP.camera.y) * OWOP.camera.zoom + 0.5;
+				var w = e[0] - s[0];
+				var h = e[1] - s[1];
+				ctx.beginPath();
+				ctx.rect(x, y, w * OWOP.camera.zoom, h * OWOP.camera.zoom);
+				ctx.globalAlpha = 1;
+				ctx.strokeStyle = "#FFFFFF";
+				ctx.stroke();
+				ctx.setLineDash([3, 4]);
+				ctx.strokeStyle = "#000000";
+				ctx.stroke();
+				ctx.globalAlpha = 0.25 + Math.sin(time / 500) / 4;
+				ctx.fillStyle = OWOP.renderer.patterns.unloaded;
+				ctx.fill();
+				ctx.setLineDash([]);
+				var oldfont = ctx.font;
+				ctx.font = "16px sans-serif";
+				var txt = (!tool.extra.clicking ? "Right click to copy " : "") + '(' + Math.abs(w) + 'x' + Math.abs(h) + ')';
+				var txtx = window.innerWidth >> 1;
+				var txty = window.innerHeight >> 1;
+				txtx = Math.max(x, Math.min(txtx, x + w * OWOP.camera.zoom));
+				txty = Math.max(y, Math.min(txty, y + h * OWOP.camera.zoom));
+
+				drawText(ctx, txt, txtx, txty, true);
+				ctx.font = oldfont;
+				ctx.lineWidth = oldlinew;
+				return 0;
+			} else {
+				ctx.beginPath();
+				ctx.moveTo(0, fxy + 0.5);
+				ctx.lineTo(window.innerWidth, fxy + 0.5);
+				ctx.moveTo(fxx + 0.5, 0);
+				ctx.lineTo(fxx + 0.5, window.innerHeight);
+
+				//ctx.lineWidth = 1;
+				ctx.globalAlpha = 1;
+				ctx.strokeStyle = "#FFFFFF";
+				ctx.stroke();
+				ctx.setLineDash([3]);
+				ctx.strokeStyle = "#000000";
+				ctx.stroke();
+
+				ctx.setLineDash([]);
+				ctx.lineWidth = oldlinew;
+				return 1;
+			}
+		});
+
+		tool.extra.start = null;
+		tool.extra.end = null;
+		tool.extra.clicking = false;
+
+		tool.setEvent('mousedown', function (mouse, event) {
+			var s = tool.extra.start;
+			var e = tool.extra.end;
+			var isInside = function isInside() {
+				return OWOP.mouse.tileX >= s[0] && OWOP.mouse.tileX < e[0] && OWOP.mouse.tileY >= s[1] && OWOP.mouse.tileY < e[1];
+			};
+			if (mouse.buttons === 1 && !tool.extra.end) {
+				tool.extra.start = [mouse.tileX, OWOP.mouse.tileY];
+				tool.extra.clicking = true;
+				tool.setEvent('mousemove', function (mouse, event) {
+					if (tool.extra.start && mouse.buttons === 1) {
+						tool.extra.end = [mouse.tileX, OWOP.mouse.tileY];
+						return 1;
+					}
+				});
+				var finish = function finish() {
+					tool.setEvent('mousemove mouseup deselect', null);
+					tool.extra.clicking = false;
+					var s = tool.extra.start;
+					var e = tool.extra.end;
+					if (e) {
+						if (s[0] === e[0] || s[1] === e[1]) {
+							tool.extra.start = null;
+							tool.extra.end = null;
+						}
+						if (s[0] > e[0]) {
+							var tmp = e[0];
+							e[0] = s[0];
+							s[0] = tmp;
+						}
+						if (s[1] > e[1]) {
+							var tmp = e[1];
+							e[1] = s[1];
+							s[1] = tmp;
+						}
+					}
+					OWOP.renderer.render(OWOP.renderer.rendertype.FX);
+				};
+				tool.setEvent('deselect', finish);
+				tool.setEvent('mouseup', function (mouse, event) {
+					if (!(mouse.buttons & 1)) {
+						finish();
+					}
+				});
+			} else if (mouse.buttons === 1 && tool.extra.end) {
+				if (isInside()) {
+					var offx = OWOP.mouse.tileX;
+					var offy = OWOP.mouse.tileY;
+					tool.setEvent('mousemove', function (mouse, event) {
+						var dx = OWOP.mouse.tileX - offx;
+						var dy = OWOP.mouse.tileY - offy;
+						tool.extra.start = [s[0] + dx, s[1] + dy];
+						tool.extra.end = [e[0] + dx, e[1] + dy];
+					});
+					var end = function end() {
+					   tool.setEvent('mouseup deselect mousemove', null);
+					};
+					tool.setEvent('deselect', end);
+					tool.setEvent('mouseup', function (mouse, event) {
+						if (!(mouse.buttons & 1)) {
+							end();
+						}
+					});
+				} else {
+					tool.extra.start = null;
+					tool.extra.end = null;
+				}
+		   } else if (mouse.buttons === 2 && tool.extra.end && isInside()) {
+				tool.extra.start = null;
+				tool.extra.end = null;
+				var x = s[0];
+				var y = s[1];
+				var w = e[0] - s[0];
+				var h = e[1] - s[1];
+				var c = document.createElement('canvas');
+				c.width = w;
+				c.height = h;
+				var ctx = c.getContext('2d');
+				var d = ctx.createImageData(w, h);
+				for (var i = y; i < y + h; i++) {
+					for (var j = x; j < x + w; j++) {
+						var pix = OWOP.misc.world.getPixel(j, i);
+						if (!pix) continue;
+						d.data[4 * ((i - y) * w + (j - x))] = pix[0];
+						d.data[4 * ((i - y) * w + (j - x)) + 1] = pix[1];
+						d.data[4 * ((i - y) * w + (j - x)) + 2] = pix[2];
+						d.data[4 * ((i - y) * w + (j - x)) + 3] = 255;
+					}
+				}
+				ctx.putImageData(d, 0, 0);
+				var pastes = OWOP.tools.allTools.pastes;
+				pastes.extra.canvas = c;
+				var oldSelect = OWOP.tools.allTools.pastes.events.select;
+				pastes.events.select = function() {
+					pastes.events.select = oldSelect;
+				};
+				OWOP.player.tool = "pastes";
+			}
+		});
+	}));
+
+		OWOP.tools.addToolObject(new OWOP.tools.class('Pastes', OWOP.cursors.paste, OWOP.fx.player.NONE, OWOP.RANK.USER, tool => {
+
+//x, y, zoom, el, sh, ismag, type, {extra}
+		const paint = (tileX, tileY) => {
+			//var tmpBuffer = new Uint32Array(protocol.chunkSize * protocol.chunkSize);
+			//var ctx = tool.extra.canvas.getContext("2d");
+			//var dat = ctx.getImageData(0, 0, tool.extra.canvas.width, tool.extra.canvas.height);
+			let div = document.createElement("div");
+			let canv = document.createElement("canvas");
+			let ctx = canv.getContext("2d");
+			canv.width = tool.extra.canvas.width;
+			canv.height = tool.extra.canvas.width;
+			ctx.drawImage(tool.extra.canvas,0,0);
+				
+			div.appendChild(canv);
+			canv.style.position = "fixed";
+			div.style.pointerEvents = "none";
+			q = {
+				x: tileX,
+				y: tileY,
+				z: 0.2,//OWOP.camera.zoom,
+				el: div,
+				sh: false,
+				ismag: false,
+				type: "paste"
+			};
+			rip.rate(Math.random()*0.1 + 0.9);
+			rip.play();
+			//OWOP.util.stickymov();
+			console.log("pasted");
+		}
+
+		tool.setEvent('mousedown', mouse => {
+			if (mouse.buttons & 0b1) {
+				if (tool.extra.canvas) {
+					paint(mouse.tileX, OWOP.mouse.tileY);
+				}
+			}
+		});
+
+		var input = document.createElement("input");
+		input.type = "file";
+		input.accept = "image/*";
+		tool.setEvent('select', () => {
+			input.onchange = event => {
+				if (input.files && input.files[0]) {
+					var reader = new FileReader();
+					reader.onload = e => {
+						var image = new Image();
+						image.onload = () => {
+							tool.extra.canvas = document.createElement("canvas");
+							tool.extra.canvas.width = image.width;
+							tool.extra.canvas.height = image.height;
+							var ctx = tool.extra.canvas.getContext("2d");
+							ctx.drawImage(image, 0, 0);
+							console.log('Loaded image');
+						};
+						image.src = e.target.result;
+					};
+					reader.readAsDataURL(input.files[0]);
+				}
+			};
+			input.click();
+		});
+	}));
+
+
 
 }, 1000)
